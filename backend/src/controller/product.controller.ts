2819e4b71b4cf8bfae66d371990b224f55402804
mleleszi <mleleszi@icloud.com>
2022-04-11 18:41:23 +0200

    blueprint by product

--- backend/src/controller/product.controller.ts
+++ backend/src/controller/product.controller.ts
@@ -1,7 +1,33 @@
-import { getRepository } from "typeorm";
+import { createQueryBuilder, getRepository } from "typeorm";
 import { Controller } from "./base.controller";
 import { Product } from "../entity/Product";
+import { Blueprint } from "../entity/Blueprint";
 
 export class ProductController extends Controller {
   repository = getRepository(Product);
+
+  getBlueprintByProduct = async (req, res) => {
+    const entityId = req.params.id;
+
+    try {
+      const entity = await this.repository.findOne({ where: { id: entityId } });
+      if (!entity) {
+        return res.status(404).json({ message: "not found" });
+      }
+
+      const blueprints = await createQueryBuilder("blueprint")
+        .select("blueprint")
+        .from(Blueprint, "blueprint")
+        .innerJoinAndSelect("blueprint.part", "part")
+        .where("blueprint.product = :product", { product: entityId })
+        .getMany();
+
+      console.log(blueprints);
+
+      res.json(blueprints);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: err.message });
+    }
+  };
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,16 +1,36 @@
 import * as express from "express";
 import { PartController } from "./controller/part.controller";
+import { ProductController } from "./controller/product.controller";
+import { BlueprintController } from "./controller/blueprint.controller";
 
 export function getRouter() {
   const router = express.Router();
 
-  const productCtrl = new PartController();
+  const partController = new PartController();
+  const productController = new ProductController();
+  const blueprintController = new BlueprintController();
 
-  router.get("/api/part", productCtrl.getAll);
-  router.get("/api/part/:id", productCtrl.getOne);
-  router.post("/api/part", productCtrl.create);
-  router.put("/api/part", productCtrl.update);
-  router.delete("/api/part/:id", productCtrl.delete);
+  router.get("/api/part", partController.getAll);
+  router.get("/api/part/:id", partController.getOne);
+  router.post("/api/part", partController.create);
+  router.put("/api/part", partController.update);
+  router.delete("/api/part/:id", partController.delete);
+
+  router.get("/api/product", productController.getAll);
+  router.get("/api/product/:id", productController.getOne);
+  router.post("/api/product", productController.create);
+  router.put("/api/product", productController.update);
+  router.delete("/api/product/:id", productController.delete);
+  router.get(
+    "/api/product/:id/blueprint",
+    productController.getBlueprintByProduct
+  );
+
+  router.get("/api/blueprint", blueprintController.getAll);
+  router.get("/api/blueprint/:id", blueprintController.getOne);
+  router.post("/api/blueprint", blueprintController.create);
+  router.put("/api/blueprint", blueprintController.update);
+  router.delete("/api/blueprint/:id", blueprintController.delete);
 
   return router;
 }
